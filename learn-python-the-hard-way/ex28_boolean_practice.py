6cac8a547d021292f653be40920e177105405add
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2023-01-02 15:41:53 -0300

    Listas

--- learn-python-the-hard-way/ex28_boolean_practice.py
+++ learn-python-the-hard-way/ex28_boolean_practice.py
@@ -1,3 +1,85 @@
 '''
 ex28-boolean-practice
 '''
+
+print("True and True")
+print(True and True)
+print("\n\n\n")
+
+print("False and True")
+print(False and True)
+print("\n\n\n")
+
+
+print("1 == 1 and 2 == 1")
+print(1 == 1 and 2 == 1)
+print("\n\n\n")
+
+print('"test" == "test"')
+print("test" == "test")
+print("\n\n\n")
+
+print("1 == 1 or 2 != 1")
+print(1 == 1 or 2 != 1)
+print("\n\n\n")
+
+print("True and 1 == 1")
+print(True and 1 == 1)
+print("\n\n\n")
+
+print("False and 0 != 0")
+print(False and 0 != 0)
+print("\n\n\n")
+
+print("True or 1 == 1")
+print(True or 1 == 1)
+print("\n\n\n")
+
+print('"test" == "testing"')
+print("test" == "testing")
+print("\n\n\n")
+
+print("1 != 0 and 2 == 1")
+print(1 != 0 and 2 == 1)
+print("\n\n\n")
+
+print('"test" != "testing"')
+print("test" != "testing")
+print("\n\n\n")
+
+print('"test" == 1')
+print("test" == 1)
+print("\n\n\n")
+
+print("not (True and False)")
+print(not (True and False))
+print("\n\n\n")
+
+print("not (1 == 1 and 0 != 1)")
+print(not (1 == 1 and 0 != 1))
+print("\n\n\n")
+
+print("not (10 == 1 or 1000 == 1000)")
+print(not (10 == 1 or 1000 == 1000))
+print("\n\n\n")
+
+print("not (1 != 10 or 3 == 4)")
+print(not (1 != 10 or 3 == 4))
+print("\n\n\n")
+
+print('not ("testing" == "testing" and "Zed" == "Cool Guy")')
+print(not ("testing" == "testing" and "Zed" == "Cool Guy"))
+print("\n\n\n")
+
+print('1 == 1 and (not ("testing" == 1 or 1 == 0))')
+print(1 == 1 and (not ("testing" == 1 or 1 == 0)))
+print("\n\n\n")
+
+print('"chunky" == "bacon" and (not (3 == 4 or 3 == 3))')
+print("chunky" == "bacon" and (not (3 == 4 or 3 == 3)))
+print("\n\n\n")
+
+print('"3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))')
+print(3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
+print("\n\n\n")
+
