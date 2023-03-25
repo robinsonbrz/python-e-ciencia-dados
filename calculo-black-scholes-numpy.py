4c3646fddb0afd7c747a9c903abbff7e7af6bce1
Robinson Enedino <renedino@geobit.com.br>
2023-03-25 15:18:45 -0300

    Calculo valor opções Black Scholes com numpy

---
+++ calculo-black-scholes-numpy.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.stats import norm
+
+def black_scholes_merton(S, K, T, r, sigma, option='call'):
+    d1 = (np.log(S/K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    
+    if option == 'call':
+        price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+    elif option == 'put':
+        price = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+    
+    return price
+
+# Definindo as variáveis
+S = 100 # Preço atual da ação
+K = 100 # Preço de exercício da opção
+r = 0.01 # Taxa livre de risco
+sigma = 0.20 # Volatilidade do ativo subjacente
+T = 1.0 # Tempo até o vencimento da opção
+
+# Calculando o preço da opção call e put
+call_price = black_scholes_merton(S, K, T, r, sigma, option='call')
+put_price = black_scholes_merton(S, K, T, r, sigma, option='put')
+
+print("Preço da opção call: ", call_price)
+print("Preço da opção put: ", put_price)
