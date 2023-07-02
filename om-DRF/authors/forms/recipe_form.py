0f9981c0e332008b46430bd7bf03a87ef9a0badf
Robinson Enedino <robinsonbrz@gmail.com>
2023-07-01 22:18:49 -0300

    feat: aula 293

--- om-DRF/authors/forms/recipe_form.py
+++ om-DRF/authors/forms/recipe_form.py
@@ -44,31 +44,4 @@ class AuthorRecipeForm(forms.ModelForm):
     def clean(self, *args, **kwargs):
         super_clean = super().clean(*args, **kwargs)
         AuthorRecipeValidator(self.cleaned_data, ErrorClass=ValidationError)
-
         return super_clean
-
-    # def clean_title(self):
-    #     title = self.cleaned_data.get('title')
-
-    #     if len(title) < 5:
-    #         self._my_errors['title'].append('Must have at least 5 chars.')
-
-    #     return title
-
-    # def clean_preparation_time(self):
-    #     field_name = 'preparation_time'
-    #     field_value = self.cleaned_data.get(field_name)
-
-    #     if not is_positive_number(field_value):
-    #         self._my_errors[field_name].append('Must be a positive number')
-
-    #     return field_value
-
-    # def clean_servings(self):
-    #     field_name = 'servings'
-    #     field_value = self.cleaned_data.get(field_name)
-
-    #     if not is_positive_number(field_value):
-    #         self._my_errors[field_name].append('Must be a positive number')
-
-    #     return field_value
