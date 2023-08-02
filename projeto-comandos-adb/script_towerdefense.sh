e6232b0249821d1f0c82de472dcbc70f650761aa
Robinson Enedino <robinsonbrz@gmail.com>
2023-08-01 22:31:44 -0300

    feat: novas coordenadas

--- projeto-comandos-adb/script_towerdefense.sh
+++ projeto-comandos-adb/script_towerdefense.sh
@@ -5,76 +5,84 @@ for i in {1..1000}
 do
     start_time=$(date +%s)
     echo "1 Tela"
-    ./adb shell input touchscreen tap 615 327
+    adb shell input touchscreen tap 615 327
     sleep 0.5 
     
     echo "2 Quadrado"
-    ./adb shell input tap 1169 274
+    adb shell input tap 1169 274
     sleep 0.5 
 
     echo "3 Arma"
-    ./adb shell input tap 999 645
+    adb shell input tap 878 645
     sleep 0.5 
 
     echo "4 Quadrado"
-    ./adb shell input touchscreen tap 1169 274
+    adb shell input touchscreen tap 1169 274
     sleep 0.5 
 
     echo "5 Upgrade"
-    ./adb shell input touchscreen tap 1374 672
+    adb shell input touchscreen tap 1374 672
     sleep 0.5 
 
-    echo "6 Next Wave"
-    ./adb shell input touchscreen tap 803 53
+    echo "6  ######################### Next Wave = 1"
+    adb shell input touchscreen tap 803 53
     sleep 0.5 
 
     echo "7 Acelera"
-    ./adb shell input touchscreen tap 1319 57
-    sleep 11 
+    adb shell input touchscreen tap 1319 57
+    sleep 5 
+
+    echo "8-1 Upgrade"
+    adb shell input touchscreen tap 1374 669
+    sleep 3
 
     echo "8 Upgrade"
-    ./adb shell input touchscreen tap 1374 669
+    adb shell input touchscreen tap 1374 669
     sleep 0.5 
 
-    echo "9 Next Wave"
-    ./adb shell input touchscreen tap 803 53
-    sleep 12 
+    echo "8 Upgrade"
+    adb shell input touchscreen tap 1374 669
+    sleep .5
+
+    echo "9  ######################### Next Wave = 2"
+    adb shell input touchscreen tap 803 53
+    sleep 8
 
     echo "10 Upgradie"
-    ./adb shell input touchscreen tap 1374 669
-    sleep 3 
+    adb shell input touchscreen tap 1374 669
+    sleep 0.02 
 
-    echo "11 Next Wave"
-    ./adb shell input touchscreen tap 803 53
-    sleep 12 
+    echo "11  ######################### Next Wave = 3"
+    adb shell input touchscreen tap 803 53
+    sleep 10 
 
     echo "12 Upgrade"
-    ./adb shell input touchscreen tap 1374 669
-    sleep 3 
+    adb shell input touchscreen tap 1374 669
+    sleep .5 
 
-    echo "13 Next Wave"
-    ./adb shell input touchscreen tap 803 53
+    echo "13  ######################### Next Wave = 4"
+    adb shell input touchscreen tap 803 53
     sleep 12 
 
     echo "14 Upgrade" 
-    ./adb shell input touchscreen tap 1374 669
+    adb shell input touchscreen tap 1374 669
     sleep 0.5 
 
-    echo "15 Next Wave" 
-    ./adb shell input touchscreen tap 803 53
+    echo "15 ######################### Next Wave = 5" 
+    adb shell input touchscreen tap 803 53
     sleep 1 
 
     echo "16 Increase Power" 
-    ./adb shell input touchscreen tap 1475 375
+    adb shell input touchscreen tap 1475 375
     sleep 2 
 
     echo "17 Upgrade" 
-    ./adb shell input touchscreen tap 1374 669
+    adb shell input touchscreen tap 1374 669
     sleep 5 
 
 
     echo "18 Reload page" 
-    ./adb shell input touchscreen tap 701 479
+    adb shell input touchscreen tap 701 479
     sleep 3 
       # Echo the count on the terminal
     echo "Loop $counter"
@@ -92,3 +100,4 @@ do
     # echo "Total time in milliseconds is $total_time"
     # echo "The elapsed time in Seconds is $total_time_seconds"
 done
+
